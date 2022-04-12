38429f23159c1318d912b9a168c796c31b9f27a8
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-04-12 18:00:38 -0300

    Cria UserController

---
+++ src/controllers/UserController.js
@@ -0,0 +1,14 @@
+const UserService = require('../services/UserService');
+
+const createUser = async (req, res) => {
+  const { displayName, email, password, image } = req.body;
+
+  try {
+    const token = await UserService.createUser(displayName, email, password, image);
+    return res.status(201).json(token);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { createUser };
