bbd4d351c0d9d345e6a9ee2937f9d86c82aa26a5
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-04-13 17:12:20 -0300

    Cria LoginController

---
+++ src/controllers/LoginController.js
@@ -0,0 +1,16 @@
+const LoginService = require('../services/LoginService');
+const { user } = require('../schemas/loginErrors');
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  
+  try {
+    const token = await LoginService.login(email, password);
+    if (!token) return res.status(user.invalid.code).json({ message: user.invalid.message });
+    return res.status(200).json(token);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { login };
