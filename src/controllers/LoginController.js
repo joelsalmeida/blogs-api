84196e93c33e1c7ee0725d7c5f7b50c9a2f4166a
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-04-14 10:19:11 -0300

    Refactoring - Passa tratamento de erro para middleware em LoginController.login

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,12 +1,12 @@
 const LoginService = require('../services/LoginService');
 const { user } = require('../schemas/loginErrors');
 
-const login = async (req, res) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
   
   try {
     const token = await LoginService.login(email, password);
-    if (!token) return res.status(user.invalid.code).json({ message: user.invalid.message });
+    if (!token) return next(user.invalid);
     return res.status(200).json(token);
   } catch (error) {
     console.log(error);
