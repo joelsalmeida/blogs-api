26ede69c5b7c363e49febbefa7f1bb168186151d
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-04-18 19:32:55 -0300

    Cria PostController

---
+++ src/controllers/PostController.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const PostService = require('../services/PostService');
+const { User } = require('../models');
+
+const createPost = async (req, res) => {
+  const { title, content, categoryIds } = req.body;
+  const { authorization } = req.headers;
+
+  try {
+    const { data: { email, password } } = jwt.verify(authorization, process.env.JWT_SECRET);
+    const { id: userId } = await User.findOne({ where: { email, password } });
+
+    const newPost = await PostService.createPost(userId, title, content, categoryIds);
+
+    return res.status(201).json(newPost);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { createPost };
