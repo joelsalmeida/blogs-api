5f9e82a65961c5979d41f6d94dbe919bf6de15b6
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-04-26 19:00:18 -0300

    Atualiza readme, remove arquivos da Trybe

--- index.js
+++ index.js
@@ -18,8 +18,3 @@ app.use('/post', postRoutes);
 app.use(errorMiddleware);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
-
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
-  response.send();
-});
